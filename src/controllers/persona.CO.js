b2b52b972ad0912e17d7b8c548afada5801f9703
fabio <fabioarredondo44@gmail.com>
2023-05-23 15:31:37 -0400

    first commit

--- src/controllers/persona.CO.js
+++ src/controllers/persona.CO.js
@@ -80,6 +80,7 @@ export const createE = async (req, res) => {
 export const Asig = async (req, res) => {
     try {
         const { idActivo, cipersona, fecha } = req.body
+        console.log(req.body)
         await consul.query('INSERT INTO asignado (id,cipersona,fechasalida) VALUES ($1,$2,$3)', [idActivo, cipersona, fecha])
         res.send('activo asignado')
     } catch (error) {
